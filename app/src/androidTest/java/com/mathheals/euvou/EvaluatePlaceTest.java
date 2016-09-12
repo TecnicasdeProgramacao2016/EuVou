5e6562293d25453481ea33f2870f9363b246e914
miguelpimentel <miguelpimentel2013@gmail.com>
2016-09-12 20:15:04 -0300

    Applying Style Sheet in EvaluatePlaecTest and UserTest

--- app/src/androidTest/java/com/mathheals/euvou/EvaluatePlaceTest.java
+++ app/src/androidTest/java/com/mathheals/euvou/EvaluatePlaceTest.java
@@ -10,14 +10,14 @@ import org.json.JSONObject;
 import dao.EvaluatePlaceDAO;
 import model.Evaluation;
 
-/**
- * Created by marlonmendes on 09/11/15.
- */
-public class EvaluatePlaceTest extends TestCase {
+
+public class EvaluatePlaceTest extends TestCase
+{
 
     private Bundle placeInfo;
 
-    public void testEvaluatePlace() {
+    public void testEvaluatePlace()
+    {
         final float GRADE = 5.0F;
         final int ID_USER = 1;
         final int ID_PLACE = 1;
@@ -26,7 +26,8 @@ public class EvaluatePlaceTest extends TestCase {
         evaluatePlaceDAO.evaluatePlace(new Evaluation(ID_PLACE, ID_USER, GRADE));
         JSONObject jsonObject = evaluatePlaceDAO.searchPlaceEvaluation(ID_PLACE, ID_USER);
 
-        try {
+        try
+        {
             float grade = new Float(jsonObject.getJSONObject("0").getString("grade"));
             int userId = jsonObject.getJSONObject("0").getInt("idUser");
             int placeId = jsonObject.getJSONObject("0").getInt("idPlace");
@@ -34,7 +35,9 @@ public class EvaluatePlaceTest extends TestCase {
             assertEquals(grade, GRADE);
             assertEquals(ID_USER, userId);
             assertEquals(ID_PLACE, placeId);
-        } catch (JSONException e) {
+        }
+        catch (JSONException e)
+        {
             e.printStackTrace();
         }
     }
