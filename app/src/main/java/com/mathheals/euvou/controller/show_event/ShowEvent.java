bb4ed4b2e980b87307a7e067aa20fb2b2d9c5946
miguelpimentel <miguelpimentel2013@gmail.com>
2016-09-14 21:30:51 -0300

    Applying style guide, correct use of if and else in ShowEvent

--- app/src/main/java/com/mathheals/euvou/controller/show_event/ShowEvent.java
+++ app/src/main/java/com/mathheals/euvou/controller/show_event/ShowEvent.java
@@ -30,10 +30,8 @@ import exception.EventEvaluationException;
 import model.EventEvaluation;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class ShowEvent extends android.support.v4.app.Fragment implements View.OnClickListener {
+public class ShowEvent extends android.support.v4.app.Fragment implements View.OnClickListener
+{
 
     private TextView eventCategoriesText;
     private EventDAO eventDAO;
@@ -59,14 +57,30 @@ public class ShowEvent extends android.support.v4.app.Fragment implements View.O
     private RatingBar ratingBar;
     private EventEvaluation eventEvaluation;
 
-    public ShowEvent() {
+    public ShowEvent()
+    {
         // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
+
+        // private TextView eventCategoriesText;
+        // private EventDAO eventDAO;
+        // private final String PRICE_COLUMN = "price";
+        // private String eventPrice;
+        // private Button showEventOnMapButton, participateButton;
+        // private String eventLongitude;
+        // private String eventLatitude;
+        // private String eventId;
+        // private final String GO = "#EUVOU";
+        // private final String NOTGO = "#NÃOVOU";
+
+        // private final Integer LOGGED_OUT = -1;
+        //private int userId;
 
         setShowEventView(inflater.inflate(R.layout.fragment_show_event, container, false));
         showEventOnMapButton = (Button) showEventView.findViewById(R.id.showEventOnMapButton);
@@ -79,20 +93,26 @@ public class ShowEvent extends android.support.v4.app.Fragment implements View.O
         JSONObject eventDATA = eventDAO.searchEventById(Integer.parseInt(eventId));
 
         setUserId(new LoginUtility(getActivity()).getUserId());
-        if(userId == LOGGED_OUT) {
+        if(userId == LOGGED_OUT)
+        {
             participateButton.setVisibility(showEventView.GONE);
         }
-        else {
+        else
+        {
             participateButton.setVisibility(View.VISIBLE);
-            if(eventDAO.verifyParticipate(userId,Integer.parseInt(eventId)) == null) {
+            if(eventDAO.verifyParticipate(userId,Integer.parseInt(eventId)) == null)
+            {
                 participateButton.setText(GO);
             }
-            else{
+            else
+            {
                 participateButton.setText(NOTGO);
             }
         }
 
-        try {
+        try
+        {
+
             String eventNameDB = eventDATA.getJSONObject("0").getString("nameEvent");
             String eventAdress = eventDATA.getJSONObject("0").getString("address");
             String eventDescription = eventDATA.getJSONObject("0").getString("description");
@@ -114,9 +134,13 @@ public class ShowEvent extends android.support.v4.app.Fragment implements View.O
             setCategoriesText(new Integer(eventId), eventCategoriesText);
             addressShow.setText(eventAdress);
 
-        } catch (JSONException ex) {
+        }
+        catch (JSONException ex)
+        {
             ex.printStackTrace();
-        } catch (NullPointerException exception) {
+        }
+        catch (NullPointerException exception)
+        {
             Toast.makeText(getActivity(), "O nome não foi encontrado", Toast.LENGTH_LONG);
         }
 
@@ -128,7 +152,8 @@ public class ShowEvent extends android.support.v4.app.Fragment implements View.O
     }
 
 
-    private String[] getEventCategoriesById(int eventId) {
+    private String[] getEventCategoriesById(int eventId)
+    {
         final String ID_CATEGORY = "idCategory";
         final String NAME_CATEGORY = "nameCategory";
         final String FIRST_COLUMN = "0";
@@ -140,22 +165,29 @@ public class ShowEvent extends android.support.v4.app.Fragment implements View.O
 
         ArrayList<String> categories = new ArrayList<>();
 
-        for(int i = 0; i < eventCategoryJSON.length(); ++i) {
-            try {
+        for(int i = 0; i < eventCategoryJSON.length(); ++i)
+        {
+            try
+            {
                 int categoryId = eventCategoryJSON.getJSONObject(Integer.toString(i)).getInt(ID_CATEGORY);
                 JSONObject categoryJSON = categoryDAO.searchCategoryById(categoryId);
                 String categoryName = categoryJSON.getJSONObject(FIRST_COLUMN).getString(NAME_CATEGORY);
                 categories.add(categoryName);
-            } catch (JSONException e) {
+            }
+            catch (JSONException e)
+            {
                 e.printStackTrace();
             }
         }
+
         String[] categoriesArray = new String[categories.size()];
         categoriesArray = categories.toArray(categoriesArray);
+
         return categoriesArray;
     }
 
-    public void setCategoriesText(int eventId, TextView eventCategoriesText){
+    public void setCategoriesText(int eventId, TextView eventCategoriesText)
+    {
         String[] eventCategories = getEventCategoriesById(eventId);
         String text = eventCategories[0];
         for(int i = 1; i < eventCategories.length; ++i)
@@ -163,7 +195,9 @@ public class ShowEvent extends android.support.v4.app.Fragment implements View.O
         eventCategoriesText.setText(text);
     }
 
-    public void setPriceText(TextView eventPriceText, String eventPrice) {
+    public void setPriceText(TextView eventPriceText, String eventPrice)
+    {
+        // private String eventPrice;
         final int PRICE = new Integer(eventPrice);
         final String REAIS_PART = Integer.toString(PRICE / 100);
         final String CENTS = Integer.toString(PRICE % 100);
@@ -173,7 +207,10 @@ public class ShowEvent extends android.support.v4.app.Fragment implements View.O
         return;
     }
 
-    private void showEventOnMap() {
+    private void showEventOnMap()
+    {
+        // private String eventLongitude;
+        //  private String eventLatitude;
         Bundle latitudeAndLongitude = new Bundle();
         latitudeAndLongitude.putStringArray("LatitudeAndLongitude", new String[]{eventLatitude, eventLongitude});
         Intent intent = new Intent(getContext(), ShowOnMap.class);
@@ -182,30 +219,40 @@ public class ShowEvent extends android.support.v4.app.Fragment implements View.O
     }
     private void markParticipate()
     {
+        // private EventDAO eventDAO;
+        // private int userId;
+
         if(eventDAO.verifyParticipate(userId,Integer.parseInt(eventId)) != null)
             Toast.makeText(getActivity(), "Heyy, você já marcou sua participação", Toast.LENGTH_SHORT).show();
-        else {
+        else
+        {
             eventDAO.markParticipate(userId, Integer.parseInt(eventId));
             Toast.makeText(getActivity(),"Salvo com sucesso" , Toast.LENGTH_SHORT).show();
         }
     }
     private void markOffParticipate()
     {
+        // private EventDAO eventDAO;
+        // private int userId;
 
         if(eventDAO.verifyParticipate(userId,Integer.parseInt(eventId)) == null)
             Toast.makeText(getActivity(), "Heyy, você já desmarcou sua participação", Toast.LENGTH_SHORT).show();
-        else{
+        else
+        {
 
             eventDAO.markOffParticipate(userId, Integer.parseInt(eventId));
             Toast.makeText(getActivity(),"Salvo com sucesso" , Toast.LENGTH_SHORT).show();
         };
     }
 
-    public void onClick(View view) {
-        switch(view.getId()) {
+    public void onClick(View view)
+    {
+        switch(view.getId())
+        {
             case R.id.showEventOnMapButton:
                 showEventOnMap();
                 break;
+
             case R.id.EuVou:
                 if(((Button)view.findViewById(R.id.EuVou)).getText().equals(GO))
                     markParticipate();
@@ -215,15 +262,19 @@ public class ShowEvent extends android.support.v4.app.Fragment implements View.O
         }
     }
 
-    public void setUserId(int userId) {
+    // private int userId;
+    public void setUserId(int userId)
+    {
         this.userId = userId;
     }
 
-    public void setIsUserLoggedIn(boolean isUserLoggedIn) {
+    public void setIsUserLoggedIn(boolean isUserLoggedIn)
+    {
         this.isUserLoggedIn = isUserLoggedIn;
     }
 
-    private void setRatingMessage(boolean isUserLoggedIn) {
+    private void setRatingMessage(boolean isUserLoggedIn)
+    {
         final String LOGGED_IN_MESSAGE = "Sua avaliação:";
         final String LOGGED_OUT_MESSAGE = "Faça login para avaliar este evento!";
         String message = isUserLoggedIn ? LOGGED_IN_MESSAGE : LOGGED_OUT_MESSAGE;
@@ -232,17 +283,22 @@ public class ShowEvent extends android.support.v4.app.Fragment implements View.O
         ratingMessage.setText(message);
     }
 
-    public void setShowEventView(View showEventView) {
+    public void setShowEventView(View showEventView)
+    {
         this.showEventView = showEventView;
     }
 
-    private void setRatingBarIfNeeded() {
+    private void setRatingBarIfNeeded()
+    {
         if(isUserLoggedIn)
             setRatingBar();
     }
 
 
-    private void setRatingBar() {
+    private void setRatingBar()
+    {
+        // private String eventId;
+        // private int userId;
         ratingBar = (RatingBar) showEventView.findViewById(R.id.ratingBar);
         ratingBar.setVisibility(View.VISIBLE);
 
@@ -250,20 +306,26 @@ public class ShowEvent extends android.support.v4.app.Fragment implements View.O
 
         JSONObject evaluationJSON = eventEvaluationDAO.searchEventEvaluation(Integer.parseInt(eventId), userId);
 
-        if(evaluationJSON!=null){
+        if(evaluationJSON!=null)
+        {
             Float evaluation = null;
-            try {
+            try
+            {
                 evaluation = new Float(evaluationJSON.getJSONObject("0").getDouble("grade"));
-            } catch (JSONException e) {
+            }
+            catch (JSONException e)
+            {
                 e.printStackTrace();
             }
 
             ratingBar.setRating(evaluation);
         }
 
-        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener()
+        {
             @Override
-            public void onRatingChanged(RatingBar arg0, float rateValue, boolean arg2) {
+            public void onRatingChanged(RatingBar arg0, float rateValue, boolean arg2)
+            {
                 setEventEvaluation(rateValue, userId, new Integer(eventId));
 
                 EventEvaluationDAO eventEvaluationDAO = new EventEvaluationDAO();
@@ -271,32 +333,55 @@ public class ShowEvent extends android.support.v4.app.Fragment implements View.O
                 eventEvaluationDAO.evaluateEvent(getEventEvaluation());
             }
         });
+
         setRatingBarStyle();
     }
 
-    public EventEvaluation getEventEvaluation() {
+    public EventEvaluation getEventEvaluation()
+    {
         return eventEvaluation;
     }
 
-    public void setEventEvaluation(Float rating, Integer userId, Integer eventId) {
-        try {
+    public void setEventEvaluation(Float rating, Integer userId, Integer eventId)
+    {
+        //private final String SUCCESSFULL_EVALUATION_MESSAGE = "Avaliação cadastrada com sucesso";
+
+        try
+        {
             this.eventEvaluation = new EventEvaluation(rating, userId, eventId);
             Toast.makeText(getActivity().getBaseContext(), SUCCESSFULL_EVALUATION_MESSAGE, Toast.LENGTH_LONG).show();
-        }catch (EventEvaluationException exception){
-            if(exception.getMessage() == EventEvaluation.EVALUATION_IS_INVALID){
+        }
+        catch (EventEvaluationException exception)
+        {
+            if(exception.getMessage() == EventEvaluation.EVALUATION_IS_INVALID)
+            {
                 Toast.makeText(getContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
             }
-            if(exception.getMessage() == EventEvaluation.EVENT_ID_IS_INVALID){
+            if(exception.getMessage() == EventEvaluation.EVENT_ID_IS_INVALID)
+            {
                 Toast.makeText(getContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
             }
-            if(exception.getMessage() == EventEvaluation.USER_ID_IS_INVALID){
+            if(exception.getMessage() == EventEvaluation.USER_ID_IS_INVALID)
+            {
                 Toast.makeText(getContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
             }
+
+            /*
+
+            if(   exception.getMessage() == EventEvaluation.EVALUATION_IS_INVALID ||
+                  exception.getMessage() == EventEvaluation.EVENT_ID_IS_INVALID ||
+                  exception.getMessage() == EventEvaluation.USER_ID_IS_INVALID )
+            {
+                Toast.makeText(getContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+            }
+
+            */
         }
     }
 
-    private void setRatingBarStyle() {
+    private void setRatingBarStyle()
+    {
 /*        LayerDrawable stars = (LayerDrawable) ratingBar.getProgressDrawable();
         stars.getDrawable(2).setColorFilter(ContextCompat.getColor(getContext(), R.color.turquesa_app), PorterDuff.Mode.SRC_ATOP);
- */   }
+ */ }
 }
